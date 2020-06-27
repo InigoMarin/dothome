51a5997d103d4e02d98a4cf42d2884aa6d489015
s1n7ax <srineshnisala@gmail.com>
2020-06-28 01:36:49 +0530

    [UPD] update permissions

--- .local/share/s1n7ax/init/startup.py
+++ .local/share/s1n7ax/init/startup.py
@@ -9,11 +9,17 @@ sys.path.append(os.path.abspath("{}/.local/share/s1n7ax/commons".format(home)))
 
 from execute import execute
 
+
+
+execute([['st']], False)
+execute([['st']], False)
 execute([['firefox']], False)
 execute([['chromium']], False)
+
 execute([['imwheel', '-b', '45']], False)
 execute([['setxkbmap', '-option', 'ctrl:nocaps']], False)
 execute([['xset', 'r', 'rate', '180', '80']], False)
 execute([['xcompmgr']], False)
-execute([['polybar_launch.sh']], False)
-execute([['indicator-kdeconnect']], False)
+execute([['wal', '-R']], False)
+# execute([['polybar_launch.sh']], False)
+# execute([['indicator-kdeconnect']], False)
