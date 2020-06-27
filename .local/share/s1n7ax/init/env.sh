2cf1d788fe875f1131664156931f57282479dd15
s1n7ax <srineshnisala@gmail.com>
2020-06-28 00:23:22 +0530

    [ADD] add env var

--- .local/share/s1n7ax/init/env.sh
+++ .local/share/s1n7ax/init/env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # this file contains all the environment variables
-
 export PATH=$PATH:~/.local/bin
+
 export EDITOR=nvim
+
+# this is required to run kdenlive appimage
+export $(dbus-launch)
