9104c902f9cfab952fcee2671a2b3b38acb0058d
s1n7ax <srineshnisala@gmail.com>
2020-06-28 15:57:20 +0530

    [ADD] add dwm status

--- .local/share/s1n7ax/init/startup.py
+++ .local/share/s1n7ax/init/startup.py
@@ -4,22 +4,23 @@ import os
 import sys
 
 home = os.environ['HOME']
+user = os.environ['USER']
 
 sys.path.append(os.path.abspath("{}/.local/share/s1n7ax/commons".format(home)))
 
 from execute import execute
-
-
 
 execute([['st']], False)
 execute([['st']], False)
 execute([['firefox']], False)
 execute([['chromium']], False)
 
+execute([['xsetroot', '-name', ' {} '.format(user)]], False)
 execute([['imwheel', '-b', '45']], False)
 execute([['setxkbmap', '-option', 'ctrl:nocaps']], False)
 execute([['xset', 'r', 'rate', '180', '80']], False)
 execute([['xcompmgr']], False)
 execute([['wal', '-R']], False)
+execute([['dwm-status']], False)
 # execute([['polybar_launch.sh']], False)
 # execute([['indicator-kdeconnect']], False)
