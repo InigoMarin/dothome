cf7ec98fced669d267b5caf15ba877ac261e6222
s1n7ax <srineshnisala@gmail.com>
2020-07-03 15:30:57 +0530

    [ADD] add sxhkd custom configuration

--- .local/share/s1n7ax/init/startup.py
+++ .local/share/s1n7ax/init/startup.py
@@ -10,17 +10,19 @@ sys.path.append(os.path.abspath("{}/.local/share/s1n7ax/commons".format(home)))
 
 from execute import execute
 
+execute([['xsetroot', '-name', ' {} '.format(user)]], False)
+
 execute([['st']], False)
 execute([['st']], False)
 execute([['firefox']], False)
 execute([['chromium']], False)
 
-execute([['xsetroot', '-name', ' {} '.format(user)]], False)
 execute([['imwheel', '-b', '45']], False)
 execute([['setxkbmap', '-option', 'ctrl:nocaps']], False)
 execute([['xset', 'r', 'rate', '180', '80']], False)
 execute([['xcompmgr']], False)
 execute([['wal', '-R']], False)
 execute([['dwm-status']], False)
+execute([['sxhkd']], False)
 # execute([['polybar_launch.sh']], False)
 # execute([['indicator-kdeconnect']], False)
